4d97b6c6b2e26fbc78be563bc04342e4aafbaa78
lspindler2509 <lisa.spindler@hotmail.de>
2023-12-14 16:03:15 +0100

    added nedrex to env, added nedrex data to biopax: protein_encoded_by_gene, disorder_associated_with_gene

--- bin/gt2biopax.py
+++ bin/gt2biopax.py
@@ -6,8 +6,20 @@ import sys
 from pathlib import Path
 from subprocess import call
 
+from nedrex.core import iter_edges
+from nedrex.core import get_nodes
+
 import graph_tool.all as gt
 from pybiopax import biopax, model_to_owl_file
+
+import nedrex
+open_url = "https://api.nedrex.net/open/"
+nedrex.config.set_url_base(open_url)
+from nedrex.core import api_keys_active, get_api_key
+
+if api_keys_active():
+    api_key = get_api_key(accept_eula=True)
+    nedrex.config.set_api_key(api_key)
 
 logger = logging.getLogger()
 
@@ -18,6 +30,53 @@ def get_uniprot_from_entrez(entrez_ids: list[str]) -> dict[str, list[str]]:
     response = requests.post("https://api.nedrex.net/open/relations/get_encoded_proteins", json={"nodes": entrez_ids})
     response.raise_for_status()
     return response.json()
+
+
+def get_nedrex_data(entrez_ids: list[str]) -> any:
+    genes = get_nodes(node_type = "gene", node_ids=entrez_ids)
+    edges = [e for e in iter_edges("gene_associated_with_disorder")]
+
+    edges_to_delete = []
+    for e in edges:
+        if e["sourceDomainId"] not in entrez_ids:
+            edges_to_delete.append(e)
+        
+    # delete all edges we are not interested in
+    for e in edges_to_delete:
+        edges.remove(e)
+    
+    disorders_to_get = set()
+    for e in edges:
+        disorders_to_get.add(e["targetDomainId"])
+    disorders_to_get = list(disorders_to_get)
+    
+    batch_size = 300  # Die Anzahl der IDs in jeder Gruppe
+
+    disorders = []
+    for i in range(0, len(disorders_to_get), batch_size):
+        batch_ids = disorders_to_get[i:i+batch_size]
+
+        # get_nodes für die aktuelle Gruppe von IDs aufrufen
+        disorder_nodes = get_nodes(node_type="disorder", node_ids=batch_ids)
+        disorders.extend(disorder_nodes)
+    
+    # Dictionary erstellen, wobei die primaryDomainId als Schlüssel verwendet wird für schnellen Zugriff
+    disorders = {disorder['primaryDomainId']: disorder for disorder in disorders}
+    genes = {gene['primaryDomainId']: gene for gene in genes}
+    return genes, disorders, edges
+    
+
+def get_associated_disorders_for_genes(entrez_ids: list[str]) -> dict[str, list[str]]:
+    genes, disorders, edges = get_nedrex_data(entrez_ids)
+    if genes and disorders and edges:
+        gene2disorder = {}
+        for edge in edges:
+            gene_id = edge["sourceDomainId"]
+            disorder_id = edge["targetDomainId"]
+            gene2disorder.setdefault(gene_id, []).append({"disorder":disorder_id, "dataSources": edge["dataSources"]})
+        return gene2disorder, genes, disorders
+    return None
+    
 
 
 class BioPAXFactory:
@@ -41,20 +100,26 @@ class BioPAXFactory:
         if not self.g:
             self.load_graph()
         if self.id_space == "entrez":
-            # entrez_ids = [f"entrez.{self.g.vp['name'][v_i]}" for v_i in self.g.get_vertices()]
-            # gene2prot = get_uniprot_from_entrez(entrez_ids)
-            # for k, v in gene2prot.items():
-            #     [self.add_protein(v_i) for v_i in v]
-            #     self.add_gene(k)
-            self.add_gene_info()
+            entrez_ids = [f"entrez.{self.g.vp['name'][v_i]}" for v_i in self.g.get_vertices()]
+            gene2prot = get_uniprot_from_entrez(entrez_ids)
+            gene2disorder, genes, disorders = get_associated_disorders_for_genes(entrez_ids)
+
+            # TODO: add disorders as soon as Biopax supports it
+
+            for k, v in gene2prot.items():
+                [self.add_protein(v_i, k) for v_i in v]
+            self.add_gene_info(gene2disorder, genes)
         elif self.id_space == "uniprot":
             self.add_protein_info()
         self.biopaxmodel = biopax.BioPaxModel(objects=self.get_bioax_objects())
 
-    def add_gene_info(self):
+    def add_gene_info(self, gene2disorder, genes):
         for v_i in self.g.get_vertices():
             entrez_id = self.g.vp["name"][v_i]
-            self.add_gene(entrez_id)
+            if "entrez." + entrez_id in gene2disorder:
+                self.add_gene(entrez_id, genes["entrez." + entrez_id] ,gene2disorder["entrez." + entrez_id])
+            else:
+                self.add_gene(entrez_id, genes["entrez." + entrez_id])
         for e_i, e_j in self.g.get_edges():
             entrez_id1 = self.g.vp["name"][e_i]
             entrez_id2 = self.g.vp["name"][e_j]
@@ -81,13 +146,17 @@ class BioPAXFactory:
     #         ["java", "-jar", "paxtools-5.3.0.jar", "validate", owl_path.name,
     #          f"{owl_path.stem}_validation.html", "notstrict"])
 
-    def add_protein(self, uniprot_id):
+    def add_protein(self, uniprot_id, gene_id = None):
         # TODO: add all the other properties
 
         # uniprot_id = self.g.vp['name'][v]
-        uniXRef = self.xRefs.setdefault(
+        uniXRef = [self.xRefs.setdefault(
             uniprot_id, biopax.UnificationXref(uid=f"{uniprot_id}.XREF", db="uniprot", id=uniprot_id)
-        )
+        )]
+        if gene_id:
+            uniXRef.append(self.xRefs.setdefault(
+                gene_id, biopax.RelationshipXref(uid=f"{gene_id}.XREF", db="NCBI GENE", id=gene_id, relationship_type="gene product")
+            ))
         entityRef = self.entityRefs.setdefault(
             uniprot_id, biopax.ProteinReference(uid=f"{uniprot_id}.REF", xref=uniXRef)
         )
@@ -104,11 +173,17 @@ class BioPAXFactory:
             uid=interaction_id, participant=[self.entities[uniprot_id1], self.entities[uniprot_id2]]
         )
 
-    def add_gene(self, entrez_id):
-        uniXRef = self.xRefs.setdefault(
-            entrez_id, biopax.UnificationXref(uid=f"{entrez_id}.XREF", db="ncbi gene", id=entrez_id)
-        )
-        self.entities[entrez_id] = biopax.Gene(uid=entrez_id, xref=uniXRef, organism=None)
+    def add_gene(self, entrez_id, gene, associated_disorders = None):
+        uniXRef = [self.xRefs.setdefault(
+            entrez_id, biopax.UnificationXref(uid=f"{entrez_id}.XREF", db="NCBI GENE", id=entrez_id)
+        )]
+        if associated_disorders is not None:
+            for disorder in associated_disorders:
+                id = disorder["disorder"]
+                uniXRef.append(self.xRefs.setdefault(
+                    id, biopax.RelationshipXref(uid=f"{id}.XREF", db="MONDO", id=id, comment=disorder["dataSources"], relationship_type="inferred-from")
+                ))
+        self.entities[entrez_id] = biopax.Gene(uid=entrez_id, xref=uniXRef, organism=None, name=[gene["displayName"]])
 
     def add_GGI(self, entrez_id1, entrez_id2):
         interaction_id = f"{entrez_id1}_{entrez_id2}"
