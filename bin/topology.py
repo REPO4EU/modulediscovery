4025af5c2f0c09f565fb735d02ed76c86d36b427
JohannesKersting <kerstingjohannes@gmail.com>
2025-04-09 12:26:10 +0200

    setting max distance to seed to inf, if a node is not connected to any seeds

--- bin/topology.py
+++ bin/topology.py
@@ -4,6 +4,7 @@ import graph_tool.all as gt
 import sys
 import argparse
 import logging
+import numpy
 
 logger = logging.getLogger()
 
@@ -49,6 +50,10 @@ def main(argv=None):
             shortest_paths = gt.shortest_distance(g, source=added_node, target=seeds)
             min_path_length = min(shortest_paths)
             max_dist_to_seed = max(max_dist_to_seed, min_path_length)
+
+        # set max_dist_to_seed to infinity if a node is not connected to any seed node
+        if max_dist_to_seed == numpy.iinfo(numpy.int32).max:
+            max_dist_to_seed = numpy.inf
     else:
         num_seeds = ""
         max_dist_to_seed = ""
